97239d3524b71b4f2bffa24fec1c7b912df76930
OhSuBin13 <149787142+OhSuBin13@users.noreply.github.com>
2024-04-30 15:35:41 +0900

    Title: 숫자들의 배수, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240430/숫자들의 배수/multiple-of-numbers.py
@@ -0,0 +1,15 @@
+n = int(input())
+cnt = 0
+arr = []
+arr.append(n)
+
+for i in range(1, 10):
+    a = arr[i-1] + arr[0]
+    arr.append(a)
+
+for elem in arr:
+    print(elem, end = " ")
+    if elem % 5 == 0:
+        cnt += 1
+    if cnt >= 2:
+        break
