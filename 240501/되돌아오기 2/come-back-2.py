4cedf66a366b6e19717227d173a50f71063370ae
OhSuBin13 <149787142+OhSuBin13@users.noreply.github.com>
2024-05-01 17:28:55 +0900

    Title: 되돌아오기 2, Time: 132ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240501/되돌아오기 2/come-back-2.py
@@ -0,0 +1,29 @@
+x, y = 0, 0
+cur_dir = 3
+#동, 남, 서, 북
+dxs, dys = [1,0,-1,0], [0,1,0,-1]
+ans = -1
+elapse_time = 0
+
+def move(word):
+    global x, y, cur_dir, elapse_time
+    if word == 'L':
+        cur_dir = (cur_dir+3)%4
+    elif word == 'R':
+        cur_dir = (cur_dir+1)%4
+    else:
+        x, y = x + dxs[cur_dir], y + dys[cur_dir]
+    elapse_time += 1
+
+def is_point(x, y):
+    if x == 0 and y == 0:
+        ans = elapse_time
+        return True
+    return False
+
+n = input()
+for i in n:
+    move(i)
+    if is_point(x,y):
+        break
+print(elapse_time)
