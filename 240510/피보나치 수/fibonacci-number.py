b007762d3097e54240b5a0b720d5d0accbd0e6dd
OhSuBin13 <149787142+OhSuBin13@users.noreply.github.com>
2024-05-10 19:33:10 +0900

    Title: 피보나치 수, Time: 136ms, Memory: 24MB, Status: Passed - CodeTree

--- 240510/피보나치 수/fibonacci-number.py
+++ 240510/피보나치 수/fibonacci-number.py
@@ -1,6 +1,13 @@
 n = int(input())
-dp = [1] * (n+1)
-for i in range(3, n+1):
-    dp[i] = dp[i-1] + dp[i-2]
 
+dp = [-1] * (n+1)
+def fibbo(n):
+    if dp[n] != -1:
+        return dp[n]
+    if n <= 2:
+        dp[n] = 1
+    else:
+        dp[n] = fibbo(n-1) + fibbo(n-2)
+    return dp[n]
+fibbo(n)
 print(dp[n])
